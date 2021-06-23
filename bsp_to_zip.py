253e5556b51170ef6715f8f58897fcd3fea23e0b
K0IN <19688162+K0IN@users.noreply.github.com>
2021-06-23 21:06:30 +0200

    Update bsp_to_zip.py

--- bsp_to_zip.py
+++ bsp_to_zip.py
@@ -7,20 +7,19 @@ if len(sys.argv) < 2:
 path = sys.argv[1]
 basename = os.path.basename(path)
   
-mapfile = open(path,"rb")
-content = mapfile.read()
-mapfile.close()
+with open(path,"rb") as mapfile:
+    content = mapfile.read()
 
 #NUL P K ETX EOT
-offset = content.find(b'\x00\x50\x4b\x03\x04') + 1
-if offset == 0:
+offset = content.find(b'\x00\x50\x4b\x03\x04') 
+if offset == -1:
   print("couldn't find packed content.")
   input()
   sys.exit()
-  
-newfile = path + "/../" + basename.replace(".bsp",".zip")
-print("writing output to " + newfile)
 
-output = open(newfile,"wb")
-output.write(content[offset:])
-output.close()
+newfile = os.path.join(path, "/../", basename.replace(".bsp",".zip"))
+print(f"writing output to {newfile}")
+
+with open(newfile,"wb") as output:
+    output.write(content[offset + 1:])
+
